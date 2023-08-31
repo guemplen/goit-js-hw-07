05bb250eb0918455372611d2236cc7ff5f12c95c
guemplen <guemplen@gmail.com>
2023-08-31 16:50:04 +0200

    Add 1.1 & 1.2 to 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function renderGallery() {
+  const galleryMarkup = galleryItems
+    .map(
+      image => `
+           <li class="gallery__item">
+                <a class="gallery__link" href="${image.original}">
+                <img
+                class="gallery__image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}"
+                />
+                </a>
+            </li>`
+    )
+    .join('');
+  gallery.innerHTML = galleryMarkup;
+}
+renderGallery();
+//console.log(galleryItems);
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    const largeImage = event.target.getAttribute('data-source');
+    //console.log(largeImage);
+  }
+});
